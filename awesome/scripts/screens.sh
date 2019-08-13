c08a1c6a3e47e7a13126b9dcc0c20ed933cb61cc
Ricardo Lopes <rykrdo.lopes@gmail.com>
2019-08-13 20:16:10 -0300

    update

--- awesome/scripts/screens.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-hdmi=$(xrandr -q | sed -n -e '/^HDMI-1/p')
-
-if [[ "${hdmi:7:1}" == "d" ]]; then
-    xrandr --output LVDS-1 --mode 1366x768 --rate 60 --output HDMI-1 --off && feh --randomize --bg-max && feh --randomize --bg-max $HOME/Imagens/*
-else
-    xrandr --output LVDS-1 --mode 1366x768 --rate 60 --output HDMI-1 --mode 1920x1080 --rate 60 --right-of LVDS-1 && feh --randomize --bg-max $HOME/Imagens/*
-fi
