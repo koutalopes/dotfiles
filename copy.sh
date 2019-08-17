3f51a7c6703382277ef4a4e61e1fb6ef17798a6f
Ricardo Lopes <rykrdo.lopes@gmail.com>
2019-08-17 05:58:16 -0300

    add awesome

--- copy.sh
+++ copy.sh
@@ -6,8 +6,9 @@ dot=$1
 
 if [[ "$dot" == "all" ]]; then
     rm -r !(README.md|copy.sh)
-    cp -r $XDG_CONFIG_HOME/{dunst,i3,mpv,neofetch,nvim,polybar,ranger,rofi,termite,compton.conf} $HOME/Scripts/dotfiles
+    cp -r $XDG_CONFIG_HOME/{awesome,dunst,i3,mpv,neofetch,nvim,polybar,ranger,rofi,termite,compton.conf} $HOME/Scripts/dotfiles
 else
+    rm -r $HOME/Scripts/dotfiles/"$dot"
     cp -r $XDG_CONFIG_HOME/"$dot" $HOME/Scripts/dotfiles
 fi
 
