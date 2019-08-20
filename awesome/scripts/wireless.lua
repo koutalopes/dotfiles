d73f78755dfb2995dc49ef0bf626b2d05505d19c
Ricardo Lopes <rykrdo.lopes@gmail.com>
2019-08-20 03:09:36 -0300

    update wireless widget

--- awesome/scripts/wireless.lua
+++ awesome/scripts/wireless.lua
@@ -14,21 +14,22 @@ local wireless_widget = wibox.widget {
     layout = wibox.container.margin(_, 0, 0, 3)
 }
 
+
 awful.widget.watch('bash -c "iw wlp9s0 link | grep signal"', 10,
     function(widget, stdout)
         local signal = string.match(stdout, "%d+")
         local signal = tonumber(signal)
 
-        if (signal <= 60 ) then
+        if (signal == nil) then
+            icon = "disconnected"
+        elseif (signal > 0 and signal <= 60 ) then
             icon = "connected-100"
         elseif (signal > 60 and signal <= 70) then
             icon = "connected-75"
         elseif (signal > 70 and signal <= 80) then
             icon = "connected-50"
         elseif (signal > 80) then
-            icon = "connected-25"
-        else
-            icon = "disconnected"  
+            icon = "connected-25"  
         end
 
         widget.icon:set_image(icon_path .. "network-wireless-" .. icon .. ".svg")
